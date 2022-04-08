52cedede53c1846ddd0c523163173067a0715b69
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-04-08 03:12:38 -0300

    adição da naaveção de meses

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { items } from './data/items';
 import { categories } from './data/categories';
 import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -16,15 +17,22 @@ const App = () => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
 
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        {/* Área de informações */}
+        <InfoArea 
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+        />
         {/* Área de inserção */}
-        <TableArea list={filteredList}></TableArea>
+        <TableArea list={filteredList}/>
       </C.Body>
     </C.Container>
   );
