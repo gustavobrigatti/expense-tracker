47b52678aa7f9f3d70ebafdb59d6201b06735575
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-04-07 22:25:53 -0300

    criação do helper de datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { getCurrentMonth } from './helpers/dateFilter';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+
   return (
     <C.Container>
       <C.Header>
